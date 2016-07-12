c1ffb39a024454090f6035faa9a319523ad2da64
Luân Trịnh <luan.trinh@dinosys.vn>
2016-07-12 17:56:59 +0700

    setup

---
+++ WMCReport/Helper/Constant/NotificationConstant.swift
@@ -0,0 +1,49 @@
+//
+//  NotificationConstant.swift
+//  WMCLoyaltySwift
+//
+//  Created by Duy Nguyen on 6/10/15.
+//  Copyright © 2015 dinosys. All rights reserved.
+//
+
+struct NotificationConstants {
+    struct Navigation {
+        static let kNotificationRequestNavigation = "kNotificationRequestNavigation"
+    }
+    
+    struct ViewController {
+        //Navigation
+        
+        static let MainNavigationController = "MainNavigationController"
+        
+        static let ProfileViewController = "ProfileViewController"
+        
+        static let TimeTrackViewController = "TimeTrackViewController"
+        
+        static let LeaveFormContainerViewController = "LeaveFormContainerViewController"
+        
+        static let LeaveRequestViewController = "LeaveRequestViewController"
+        
+        static let LeaveBalanceViewController = "LeaveBalanceViewController"
+        
+        static let SWRevealViewController = "SWRevealViewController"
+        
+        static let LoginViewController = "LoginViewController"
+        
+        static let LeaveManagementEmployeeViewController = "LeaveManagementEmployeeViewController"
+        
+        static let LeaveManagementAdminViewController = "LeaveManagementAdminViewController"
+        
+        static let ApprovalViewController = "ApprovalViewController"
+        
+        static let AttendanceViewController = "AttendanceViewController"
+        
+        static let AttendanceAdminViewController = "AttendanceAdminViewController"
+        
+        static let AttendanceEmployeeViewController = "AttendanceEmployeeViewController"
+        
+        static let AttendanceAccessViewController = "AttendanceAccessViewController"
+        
+        static let QRImageCodeViewController = "QRImageCodeViewController"
+    }
+}
