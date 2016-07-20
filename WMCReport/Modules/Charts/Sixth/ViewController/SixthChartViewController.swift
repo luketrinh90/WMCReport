da9b2148c823ebe570dcc781aff8afb558c98900
Luân Trịnh <luan.trinh@dinosys.vn>
2016-07-20 10:14:06 +0700

    bug fixes

---
+++ WMCReport/Modules/Charts/Sixth/ViewController/SixthChartViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SixthChartViewController.swift
+//  WMCReport
+//
+//  Created by Luân Trịnh on 7/20/16.
+//  Copyright © 2016 Luân Trịnh. All rights reserved.
+//
+
+import Cocoa
+
+class SixthChartViewController: UIViewController {
+
+}
