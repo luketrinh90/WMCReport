b07626d6b95bd008b1ef9eb8ede80834a280bf7b
Luân Trịnh <luan.trinh@dinosys.vn>
2016-08-12 17:04:18 +0700

    update chart 10

---
+++ WMCReport/Modules/Charts/Eighth/CustomCell/EighthChartCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EighthChartCollectionViewCell.swift
+//  WMCReport
+//
+//  Created by Luân Trịnh on 8/12/16.
+//  Copyright © 2016 Luân Trịnh. All rights reserved.
+//
+
+import UIKit
+
+class EighthChartCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var labelNumber: UILabel!
+    @IBOutlet weak var labelDescription: UILabel!
+}
--- WMCReport/Modules/Charts/Eighth/ViewController/EighthChartViewController.swift
+++ WMCReport/Modules/Charts/Eighth/ViewController/EighthChartViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class EighthChartViewController: UIViewController {
-
+class EighthChartViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
     @IBOutlet weak var btnMenu: UIButton!
     
     override func viewDidLoad() {
@@ -25,5 +25,55 @@ class EighthChartViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.hidden = true
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("eighthChartCollectionViewCell", forIndexPath: indexPath) as! EighthChartCollectionViewCell
+        
+        
+        if indexPath.row == 0 {
+            cell.labelDescription.text = "TODAY'S REVENUE"
+            cell.labelNumber.text = "17,325,000"
+            cell.backgroundColor = UIColor(red: 0/255, green: 172/255, blue: 172/255, alpha: 1.0).colorWithAlphaComponent(0.9)
+        } else if indexPath.row == 1 {
+            cell.labelDescription.text = "TODAY'S TRANSACTION"
+            cell.labelNumber.text = "15"
+            cell.backgroundColor = UIColor(red: 0/255, green: 172/255, blue: 172/255, alpha: 1.0).colorWithAlphaComponent(0.85)
+        } else if indexPath.row == 2 {
+            cell.labelDescription.text = "YESTERDAY'S REVENUE"
+            cell.labelNumber.text = "15,294,963"
+            cell.backgroundColor = UIColor(red: 52/255, green: 143/255, blue: 226/255, alpha: 1.0).colorWithAlphaComponent(0.9)
+        } else if indexPath.row == 3 {
+            cell.labelDescription.text = "YESTERDAY'S TRANSACTION"
+            cell.labelNumber.text = "23"
+            cell.backgroundColor = UIColor(red: 52/255, green: 143/255, blue: 226/255, alpha: 1.0).colorWithAlphaComponent(0.85)
+        } else if indexPath.row == 4 {
+            cell.labelDescription.text = "CURRENT MONTH'S REVENUE"
+            cell.labelNumber.text = "658,242,356"
+            cell.backgroundColor = UIColor(red: 114/255, green: 124/255, blue: 182/255, alpha: 1.0).colorWithAlphaComponent(0.9)
+        } else if indexPath.row == 5 {
+            cell.labelDescription.text = "CURRENT MONTH'S TRANSACTION"
+            cell.labelNumber.text = "126"
+            cell.backgroundColor = UIColor(red: 114/255, green: 124/255, blue: 182/255, alpha: 1.0).colorWithAlphaComponent(0.85)
+        } else if indexPath.row == 6 {
+            cell.labelDescription.text = "LAST MONTH'S REVENUE"
+            cell.labelNumber.text = "48,654,141,000"
+            cell.backgroundColor = UIColor(red: 255/255, green: 91/255, blue: 87/255, alpha: 1.0).colorWithAlphaComponent(0.9)
+        } else if indexPath.row == 7 {
+            cell.labelDescription.text = "LAST MONTH'S TRANSACTION"
+            cell.labelNumber.text = "50,145"
+            cell.backgroundColor = UIColor(red: 255/255, green: 91/255, blue: 87/255, alpha: 1.0).colorWithAlphaComponent(0.85)
+        }
+        
+        
+        return cell
     }
 }
